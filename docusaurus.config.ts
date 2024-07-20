337d2adafeb6e2ed68f661e87ab819a206603fee
gretmn102 <erlgreywatson@gmail.com>
2024-07-20 11:47:18 +0300

    feat: set theme by user system preferences

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -126,6 +126,11 @@ const config: Config = {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
     },
+    colorMode: {
+      defaultMode: 'dark',
+      disableSwitch: false,
+      respectPrefersColorScheme: true,
+    },
   } satisfies Preset.ThemeConfig,
 };
 
