245ba6372af1d29ef42040b6e0874849bdc6fda7
aleksversus <aleksversus@mail.ru>
2024-08-03 22:02:45 +0500

    fix(prism-qsp): universalize structure of syntax highlighting

--- src/theme/qsp-syntax/prism-qsp.js
+++ src/theme/qsp-syntax/prism-qsp.js
@@ -1,66 +1,32 @@
+
+let kw_operators = 'inclib|freelib|addqst|killqst|openqst|opengame|savegame|addobj|delobj|killobj|unsel|unselect|cls|cla|delact|cmdclr|cmdclear|play|close|all|jump|dynamic|goto|gt|xgoto|xgt|gosub|gs|menu|exit|showobjs|showstat|showacts|showinput|refint|copyarr|killvar|killall|view|msg|settimer|wait|exec'
+let kw_controls = 'act|if|elseif|else|loop|while|step|end'
+let kw_text_operators = '\\*(pl?|nl|clr|clear)|(pl?|nl|clr|clear)'
+
+let functions_string = '\\$(desc|user_text|usrtxt|maintxt|stattxt|curloc|selobj|selact|mid|(u|l)case|trim|replace|str|strfind|input|qspver|curacts|getobj|iif|dyneval|func|max|min|arritem)\\b'
+let functions_num = '\\b(loc|obj|isnum|isplay|len|val|instr|strcomp|strpos|arrsize|arrpos|arrcomp|msecscount|rgb|countobj|ra?nd|iif|dyneval|func|max|min|arritem)\\b'
+
+let system_vars_string = '\\$(counter|ongload|ongsave|onnewloc|onactsel|onobjsel|onobjadd|onobjdel|usercom|fname|backimage|args|result)\b'
+let system_vars_num = '\\b(nosave|disablescroll|disablesubex|debug|usehtml|(b|f|l)color|fsize|args|result)\\b'
+
 Prism.languages.qsp = {
 	'comment': {
 		pattern: /((^\s*?)|(\&\s*?))(!.*$)/m,
-		lookbehind: false, // поиск обратно
-		greedy: true // жадный
-	},
-	// 'string-interpolation': {
-	// 	pattern: /(?:f|fr|rf)(?:("""|''')[\s\S]*?\1|("|')(?:\\.|(?!\2)[^\\\r\n])*\2)/i,
-	// 	greedy: true,
-	// 	inside: {
-	// 		'interpolation': {
-	// 			// "{" <expression> <optional "!s", "!r", or "!a"> <optional ":" format specifier> "}"
-	// 			pattern: /((?:^|[^{])(?:\{\{)*)\{(?!\{)(?:[^{}]|\{(?!\{)(?:[^{}]|\{(?!\{)(?:[^{}])+\})+\})+\}/,
-	// 			lookbehind: true,
-	// 			inside: {
-	// 				'format-spec': {
-	// 					pattern: /(:)[^:(){}]+(?=\}$)/,
-	// 					lookbehind: true
-	// 				},
-	// 				'conversion-option': {
-	// 					pattern: /![sra](?=[:}]$)/,
-	// 					alias: 'punctuation'
-	// 				},
-	// 				rest: null
-	// 			}
-	// 		},
-	// 		'string': /[\s\S]+/
-	// 	}
-	// },
-	// 'triple-quoted-string': {
-	// 	pattern: /(?:[rub]|br|rb)?("""|''')[\s\S]*?\1/i,
-	// 	greedy: true,
-	// 	alias: 'string'
-	// },
-	'string': {
-		pattern: /("|')(?:\\.|(?!\1)[^\\\r\n])*\1/i,
+		lookbehind: false,
 		greedy: true
 	},
-	// 'function': {
-	// 	pattern: /((?:^|\s)def[ \t]+)[a-zA-Z_]\w*(?=\s*\()/g,
-	// 	lookbehind: true
-	// },
-	// 'class-name': {
-	// 	pattern: /(\bclass\s+)\w+/i,
-	// 	lookbehind: true
-	// },
-	// 'decorator': {
-	// 	pattern: /(^[\t ]*)@\w+(?:\.\w+)*/m,
-	// 	lookbehind: true,
-	// 	alias: ['annotation', 'punctuation'],
-	// 	inside: {
-	// 		'punctuation': /\./
-	// 	}
-	// },
-	'builtin': /\b(?:\$(desc|user_text|usrtxt|maintxt|stattxt|curloc|selobj|selact|mid|(u|l)case|trim|replace|str|strfind|input|qspver|curacts|getobj))\b/i,
-	'boolean': /\b(?:False|None|True)\b/,
+	'string': {
+		pattern: /("|')(?:[\s\S]*?)\1/i,
+		lookbehind: false,
+		greedy: true
+	},
+	'sys-variable': RegExp(`(?:${system_vars_string}|${system_vars_num})`, 'i'),
+	'builtin': RegExp(`(?:(${functions_string}|${functions_num}))`, 'i'),
 	'number': /\d+/,
+	'keyword': RegExp(`(?:((${kw_text_operators})\b)|${kw_operators}|${kw_controls})`, 'i'),
+	'start-location': /^\#\s*\S+.*$/im,
+	'end-location': /^\-.*$/im,
 	'operator': /[<>\+\-\*\/]=|<>|\+|-|<|>|=|\/|\*|!|and|or|mod/i,
-	'keyword': /(?:(\*(pl?|nl|clr|clear))|(pl?|nl|clr|clear))\b|\b(?:inclib|freelib|addqst|killqst|openqst|opengame|savegame|addobj|delobj|killobj|unsel|unselect|cls|cla|delact|cmdclr|cmdclear|play|close|all|jump|dynamic|goto|gt|xgoto|xgt|gosub|gs|menu|exit|showobjs|showstat|showacts|showinput|refint|copyarr|killvar|killall|view|msg|settimer|wait|exec)\b/i,
 	'punctuation': /[{}[\];(),:]/,
-	'variable': /[\$\%][\wа-я]+|[\wа-я]+/i,
+	'user-variable': /[\$\%][\wа-я]+|[\wа-я]+/i,
 };
-
-// Prism.languages.qsp['string-interpolation'].inside['interpolation'].inside.rest = Prism.languages.qsp;
-
-// Prism.languages.py = Prism.languages.qsp
